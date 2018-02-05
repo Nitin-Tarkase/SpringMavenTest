5470b0745edcb6dbc8cab1ce70a540f9a8a2b7aa
nitin <nitinblue98@gmail.com>
2018-02-05 23:31:12 +0530

    Commit added print statement

--- src/main/java/com/shapes/TestDelete1.java
+++ src/main/java/com/shapes/TestDelete1.java
@@ -5,6 +5,7 @@ public class TestDelete1 {
 	public static void main(String[] args) {
 		try {
 			Class.forName("com.shapes.Circle");
+			System.out.println("after class dot for name");
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
